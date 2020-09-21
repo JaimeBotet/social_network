c620ca419de1f425d895963f5ccb4e960e1bd6aa
albert-grandes <agrandes11@outlook.es>
2020-09-21 16:54:00 +0200

    #Small-Change: Some changes colors app.css and logos

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,7 +1,8 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            <!-- <x-jet-authentication-card-logo /> -->
+            <img class="login_logo" src='https://vignette.wikia.nocookie.net/leagueoflegends/images/5/54/Bandle_City_Crest_icon.png'>
         </x-slot>
 
         <div class="mb-4 text-sm text-gray-600">
