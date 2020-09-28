273c1bdf027f28889928d28f9e83b72bc06dac0b
albert-grandes <agrandes11@outlook.es>
2020-09-28 17:38:51 +0200

    #Small change -> Comments active and deactive ok

--- public/js/index.js
+++ public/js/index.js
@@ -30254,39 +30254,45 @@ $(function () {
 
 function commentButton() {
   $('.bandle_button').click(function (event) {
-    //We have to do a condition if is active or not
     var dataURL = $(this).attr("data-src");
-    var dataID = $(this).attr("data-post");
-    $.ajax({
-      headers: {
-        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-      },
-      url: dataURL
-    }).done(function (data) {
-      var dataArray = JSON.parse(data);
-      dataArray.forEach(function (element, i) {
-        var content = element.content;
-        var author = element.author_id;
-        $("#comment_container_id_".concat(dataID)).append($("<div>", {
-          "class": 'container_comment m-2'
-        }).append($("<p>", {
-          text: content
-        })).append($("<p>", {
-          text: author,
-          "class": 'flex justify-end text-sm'
-        }).click(function () {
-          alert("We have to put redirect to correct web...");
-        })));
+    var dataID = $(this).attr("data-post"); //We have to do a condition if is active or not
+
+    if ($(this).hasClass('showing')) {
+      $("#comment_container_id_".concat(dataID)).text('');
+      $(this).removeClass('showing'); // $(`#comment_container_id_${dataID}`).html()='';
+    } else {
+      $(this).addClass('showing');
+      $.ajax({
+        headers: {
+          'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        },
+        url: dataURL
+      }).done(function (data) {
+        var dataArray = JSON.parse(data);
+        dataArray.forEach(function (element, i) {
+          var content = element.content;
+          var author = element.author_id;
+          $("#comment_container_id_".concat(dataID)).append($("<div>", {
+            "class": 'container_comment m-2'
+          }).append($("<p>", {
+            text: content
+          })).append($("<p>", {
+            text: author,
+            "class": 'flex justify-end text-sm'
+          }).click(function () {
+            alert("We have to put redirect to correct web...");
+          })));
+        });
+        $(".comment_container_id_".concat(dataID)).append($("<p>", {
+          "class": 'container_comment'
+        }));
+        console.log(JSON.parse(data));
+      }).fail(function () {
+        alert("error");
       });
-      $(".comment_container_id_".concat(dataID)).append($("<p>", {
-        "class": 'container_comment'
-      }));
-      alert("success");
-      console.log(JSON.parse(data));
-    }).fail(function () {
-      alert("error");
-    });
-    alert($(this).attr("data-src"));
+    }
+
+    console.log($(this).attr("data-src"));
   });
 }
 
--- resources/js/frontend/dashboard.js
+++ resources/js/frontend/dashboard.js
@@ -5,43 +5,46 @@ $(function(){
 
 function commentButton() {
     $('.bandle_button').click(function(event){
-        //We have to do a condition if is active or not
-
-
         const dataURL = $(this).attr("data-src");
         const dataID = $(this).attr("data-post");
-        $.ajax({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            },
-            url: dataURL
-        }).done(function(data) {
-            let dataArray = JSON.parse(data)
-            dataArray.forEach((element,i) => {
-                let content=element.content
-                let author=element.author_id
-                $(`#comment_container_id_${dataID}`).append(
-                    $("<div>", {class: 'container_comment m-2'})
-                    .append(
-                        $("<p>", {text: content})
+        //We have to do a condition if is active or not
+        if($(this).hasClass('showing')) {
+            $(`#comment_container_id_${dataID}`).text('')
+            $(this).removeClass('showing')
+            // $(`#comment_container_id_${dataID}`).html()='';
+        } else {
+            $(this).addClass('showing')
+            $.ajax({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                },
+                url: dataURL
+            }).done(function(data) {
+                let dataArray = JSON.parse(data)
+                dataArray.forEach((element,i) => {
+                    let content=element.content
+                    let author=element.author_id
+                    $(`#comment_container_id_${dataID}`).append(
+                        $("<div>", {class: 'container_comment m-2'})
+                        .append(
+                            $("<p>", {text: content})
+                        )
+                        .append(
+                            $("<p>", {text: author, class: 'flex justify-end text-sm'}).click(function(){
+                                alert("We have to put redirect to correct web...")
+                            })
+                        )
                     )
-                    .append(
-                        $("<p>", {text: author, class: 'flex justify-end text-sm'}).click(function(){
-                            alert("We have to put redirect to correct web...")
-                        })
-                    )
+                });
+                $(`.comment_container_id_${dataID}`).append(
+                    $("<p>", {class: 'container_comment'})
                 )
+                console.log(JSON.parse(data))
+            })
+            .fail(function() {
+                alert( "error" );
             });
-            $(`.comment_container_id_${dataID}`).append(
-                $("<p>", {class: 'container_comment'})
-            )
-            alert( "success" );
-            console.log(JSON.parse(data))
-        })
-        .fail(function() {
-            alert( "error" );
-        })
-        ;
-        alert($(this).attr("data-src"))
+        }
+        console.log($(this).attr("data-src"))
     })
 }
